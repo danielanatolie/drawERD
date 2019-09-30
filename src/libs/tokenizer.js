8da9e8a15962121eff5fb709d36903762eee3254
Ari Khuyagbaatar <thearikh@gmail.com>
2019-09-30 10:04:59 -0700

    implement the rest of the tokenizer class

--- src/libs/tokenizer.js
+++ src/libs/tokenizer.js
@@ -1,36 +1,60 @@
+const LITERALS = [
+  'Entity',
+  'Name:',
+  'Unique attributes',
+  'Non-unique attributes',
+  'end'
+];
 export class Tokenizer {
   constructor(input) {
     this.input = input;
-    this.literals = [
-      'Entity',
-      'Name:',
-      'Unique attributes',
-      'Non-unique attributes',
-      'end'
-    ];
+    this.literals = LITERALS;
     this.tokens = [];
+    this.currentToken = 0;
   }
 
   tokenize() {
     let tempInput = this.input;
     // TODO: replace new lines with an empty string
-    tempInput = tempInput.replace(':', '');
-    console.log('after removing colons');
+    tempInput = tempInput.replace(/:/g, '');
 
     for (let lit of this.literals) {
-      tempInput = tempInput.replace(lit, lit + '$');
+      tempInput = tempInput.replace(lit, '$' + lit + '$');
     }
 
     tempInput = tempInput.replace(/\s/g, '');
-    console.log('this.input', tempInput);
-
-    this.tokens = tempInput.split('$');
+    this.tokens = tempInput.split('$').filter(s => s !== '');
     console.log('my tokens', this.tokens);
   }
-  checkNext() {}
-  getNext() {}
-  checkToken() {}
-  getAndCheckNext() {}
+  checkNext() {
+    let token = '';
+    if (this.currentToken < this.tokens.length) {
+      token = this.tokens[currentToken];
+    } else {
+      token = 'NO_MORE_TOKENS';
+    }
+    return token;
+  }
+  getNext() {
+    if (this.currentToken < this.tokens.length) {
+      token = this.tokens[currentToken];
+      this.currentToken++;
+    } else {
+      token = 'NULLTOKEN';
+    }
+    return token;
+  }
+  checkToken(regex) {
+    let s = this.checkNext();
+    console.log('comparing:' + s + 'to' + regex);
+    return s.match(regex);
+  }
+  getAndCheckNext(regex) {
+    let s = this.getNext();
+    if (!s.match(regex)) throw new Error('something went wrong...');
+    console.log('matched' + s + 'to' + regex);
+    return s;
+  }
 }
 
 var input =
