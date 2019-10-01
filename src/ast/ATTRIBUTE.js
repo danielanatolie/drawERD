8fbcf7551a7837d51adead470f99d400e4572a7a
Jasmin <jasmingoh9@gmail.com>
2019-10-01 16:02:45 -0700

    Update ATTRIBUTE.js

---
+++ src/ast/ATTRIBUTE.js
@@ -0,0 +1,17 @@
+class ATTRIBUTE {
+    constructor(isUnique) {
+        this.isUnique = isUnique;
+    }
+
+    parse() {
+        // this.name // attribute name
+    }
+
+    evaluate() {
+        // calls edge.evaluate()
+    }
+
+    typeCheck() {
+        // phase 2
+    }
+}
