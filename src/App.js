d1aa53819f8821d33044317920ea38f892b783f4
Ari Khuyagbaatar <thearikh@gmail.com>
2019-10-06 12:49:34 -0700

    implement description component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
-
+// import mermaid from 'mermaid';
 import Input from './components/Input';
+import { Description } from './components/Description';
+
 import './App.css';
 
 class App extends Component {
   render() {
+    // TOKENIZE HERE
+
     return (
       <div className="App">
         <header>
@@ -13,6 +17,7 @@ class App extends Component {
 
         <body>
           <div className="container">
+            <Description />
             <Input />
           </div>
         </body>
